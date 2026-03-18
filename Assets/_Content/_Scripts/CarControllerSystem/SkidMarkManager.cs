8b4b4b5287427a9cc98e5d1e2fe7274f294cfe95
ValeriiDobrotskyi <valerii.dobrotsky@midnight.works>
2026-03-18 22:23:37 +0200

    Added Skid marks

---
+++ Assets/_Content/_Scripts/CarControllerSystem/SkidMarkManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _Scripts.CarControllerSystem {
+    public class SkidMarkManager : MonoBehaviour {
+        [SerializeField] private Wheel _wheel;
+        [SerializeField] private TrailRenderer _trailRenderer;
+
+        private void FixedUpdate() {
+            _trailRenderer.emitting = _wheel.IsSlipping;
+        }
+    }
+}
--- Assets/_Content/_Scripts/CarControllerSystem/Wheel.cs
+++ Assets/_Content/_Scripts/CarControllerSystem/Wheel.cs
@@ -3,16 +3,25 @@
 namespace _Scripts.CarControllerSystem {
     [RequireComponent(typeof(WheelCollider))]
     public class Wheel : MonoBehaviour {
+        private const float GROUND_SLIP = 0.275f;
+
+        [SerializeField] private Rigidbody _rb;
         [SerializeField] private GameObject _model;
+
         [SerializeField, HideInInspector] private WheelCollider _collider;
         private Vector3 _worldPosition;
         private Quaternion _worldRotation;
+        private WheelHit _wheelHit;
+        private float _wheelSlipAmountForward = 0f;
+        private float _wheelSlipAmountSideways = 0f;
+        private float _totalSlip = 0f;
 
         [field: SerializeField] public bool CanPower { private set; get; }
         [field: SerializeField] public bool CanBrake { private set; get; }
         [field: SerializeField] public bool CanSteer { private set; get; }
 
         public float SteerAngle => _collider.steerAngle;
+        public bool IsSlipping { private set; get; }
 
         public void ApplyMotorTorque(float torque) {
             _collider.motorTorque = torque;
@@ -30,14 +39,37 @@ namespace _Scripts.CarControllerSystem {
             AlignWheels();
         }
 
+        private void FixedUpdate() {
+            _collider.GetGroundHit(out _wheelHit);
+            CheckSlipping();
+        }
+
         private void AlignWheels() {
             _collider.GetWorldPose(out _worldPosition, out _worldRotation);
             _model.transform.SetPositionAndRotation(_worldPosition, _worldRotation);
         }
 
+        private void CheckSlipping() {
+            if (_collider.isGrounded) {
+                _wheelSlipAmountForward = Mathf.Abs(_wheelHit.forwardSlip);
+                _wheelSlipAmountSideways = Mathf.Abs(_wheelHit.sidewaysSlip);
+            }
+            else {
+                _wheelSlipAmountForward = 0f;
+                _wheelSlipAmountSideways = 0f;
+            }
+
+            _totalSlip = Mathf.Lerp(_totalSlip, ((_wheelSlipAmountSideways + _wheelSlipAmountForward) / 2f),
+                Time.fixedDeltaTime * 5f);
+
+            IsSlipping = _totalSlip > GROUND_SLIP && _rb.linearVelocity.magnitude > 1;
+        }
+
+#if UNITY_EDITOR
         private void OnValidate() {
             if (_collider == null)
                 _collider = GetComponent<WheelCollider>();
         }
+#endif
     }
 }
