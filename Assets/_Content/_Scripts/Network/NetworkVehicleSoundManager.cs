3b6efa3010ca2b99c0f60cd5aa05e51525966282
ValeriiDobrotskyi <valerii.dobrotsky@midnight.works>
2026-03-19 21:15:57 +0200

    Added syncing of Engine Sound between players

---
+++ Assets/_Content/_Scripts/Network/NetworkVehicleSoundManager.cs
@@ -0,0 +1,21 @@
+﻿using _Scripts.CarControllerSystem;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace _Scripts.Network {
+    public class NetworkVehicleSoundManager : NetworkBehaviour {
+        [SerializeField] private Vehicle _vehicle;
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AnimationCurve _pitchCurve;
+
+        private void Update() {
+            if (IsOwner && NetworkManager.Singleton != null)
+                SetPitchRpc(_pitchCurve.Evaluate(Mathf.Min(_vehicle.CurrentSpeed / _vehicle.MaxSpeed, 1)));
+        }
+
+        [Rpc(SendTo.Everyone)]
+        private void SetPitchRpc(float pitch) {
+            _audioSource.pitch = pitch;
+        }
+    }
+}
